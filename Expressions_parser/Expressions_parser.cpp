21962d91f3abc709ea750a6ff113022d6578acbb
maverick-12 <maverick-12@users.noreply.github.com>
2021-06-02 22:42:47 +0200

    basic structure 1

--- Expressions_parser/Expressions_parser.cpp
+++ Expressions_parser/Expressions_parser.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool exit = false;
+    std::string input;
+
+    do {
+        input = "";
+        std::cout << "\n\nInsert expression\n";
+        std::cin >> input;
+        std::cout << input;
+        if (input.compare("\nquit")) {
+            std::cout << "\nExiting..";
+            exit = true;
+        }
+        else {
+
+        }
+
+    } while (!exit);
+
+    std::cout << "\nExited";
+    return 0;
 }
