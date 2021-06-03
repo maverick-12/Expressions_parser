8887dd9a8ce7b6661e6124175ddb8f560fd618bf
maverick-12 <maverick-12@users.noreply.github.com>
2021-06-03 18:09:16 +0200

    Revert "basic structure 1"
    This reverts commit 4506742ca1a7650865e74c18341c3edeae52efc2.

--- Expressions_parser/Expressions_parser.cpp
+++ Expressions_parser/Expressions_parser.cpp
@@ -1,30 +1,10 @@
 #include <iostream>
-#include <string>
 
 class Parser;
 
 int main()
 {
-    bool exit = false;
-    std::string input;
-
-    do {
-        input = "";
-        std::cout << "\n\nInsert expression\n";
-        std::cin >> input;
-        std::cout << input;
-        if (input.compare("\nquit")) {
-            std::cout << "\nExiting..";
-            exit = true;
-        }
-        else {
-
-        }
-
-    } while (!exit);
-
-    std::cout << "\nExited";
-    return 0;
+    std::cout << "Hello World!\n";
 }
 
 
