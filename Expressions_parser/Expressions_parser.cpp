7a0b4b1aaa8efffe31ea4b2c2c2ed1f8ad89692c
maverick-12 <maverick-12@users.noreply.github.com>
2021-06-03 18:09:39 +0200

    Revert "added class Parser"
    This reverts commit e2ab0121f2753137e2ff8374f208f18056fb6762.

--- Expressions_parser/Expressions_parser.cpp
+++ Expressions_parser/Expressions_parser.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 
-class Parser;
-
 int main()
 {
     std::cout << "Hello World!\n";
-}
-
-
-class Parser {
-public:
-    Parser();
-    ~Parser();
-
-private:
-
-};
-
-Parser::Parser() {
-}
-
-Parser::~Parser() {
 }
